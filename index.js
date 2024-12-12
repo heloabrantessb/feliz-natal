48b154c0023ea626c4b10290ba17fd653e53e4c6
heloabrantessb <heloisa.abrantesdev@gmail.com>
2024-12-11 23:22:01 -0300

    bolas piscando

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-let pisca = document.querySelector('.red');
+let listaBolas = document.querySelectorAll(".circle");
 
-function mudarEstado(){
-   if (pisca.classList.contains("on")) {
-    pisca.classList.remove("on");
-    pisca.classList.add("off");
-   }else{
-    pisca.classList.add("on")
-    pisca.classList.remove("off");
-   }
+console.log(listaBolas);
+
+function mudarEstado(bola, intervalo) {
+    
+    console.log(bola)
+    setTimeout(() => {
+      if (bola.classList.contains("on")) {
+        bola.classList.remove("on");
+        bola.classList.add("off");
+      } else {
+        bola.classList.add("on");
+        bola.classList.remove("off");
+      }
+
+      setTimeout(mudarEstado(bola, intervalo), intervalo)
+    }, intervalo);
 }
 
-function piscar(){
-    setInterval(mudarEstado, 1000);
+function Ola(){
+    console.log("Hello");
+    
+}
+// function piscar(){
+
+//         let piscando = setInterval(mudarEstado(), 1000);
+
+//     }
+
+//     setTimeout(() => {
+//         clearInterval(piscando);
+//     }, 10000)
+
+function piscar() {
+  listaBolas.forEach((bola) => {
+    console.log(bola)
+    let intervalo = Math.floor(Math.random() * 200) + 200
+    mudarEstado(bola, intervalo)
+  });
 }
 
-let piscando = piscar();
+// let piscando = piscar();
 
-setTimeout(() => {
-    clearInterval(piscando);
-}, 10000)
